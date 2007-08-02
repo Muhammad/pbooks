6aaf96896ccc799c02fa1db3acf3162392437db4
Albert Lash <albert.lash@docunext.com>
2007-08-02 18:45:05 +0000

    update

--- lib/php/runtime.php
+++ lib/php/runtime.php
@@ -1,12 +1,18 @@
 <?php
 
-//This is different depending on which web/file.php is called. 
-require_once(PROJECT_ROOT.DIRECTORY_SEPARATOR.'apps'.DIRECTORY_SEPARATOR.APP_NAME.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'config.php');
 
+
+    
+//This is different depending on which web/file.php is called. 
+$app_config=PROJECT_ROOT.DIRECTORY_SEPARATOR.'apps'.DIRECTORY_SEPARATOR.APP_NAME.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'config.php';
+
+if(is_file($app_config)) { 
+    require_once($app_config);
+}
 
 $path = $_SERVER['SCRIPT_NAME'];
 $path_prefix = dirname($_SERVER['SCRIPT_NAME']);
-$app_prefix = "acc/".APP_NAME."/";
+//$app_prefix = "acc/".APP_NAME."/";
 $link_prefix = $path."?nid=".$app_prefix;
 // URL settings
 Flow::add("path_prefix",$path_prefix,false);
