2113b816caf83c3d2d0cfd6a2e4cab255a705678
Albert Lash <albert.lash@docunext.com>
2007-08-06 16:03:54 +0000

    cleaning up runtime nodes, consolidating into a single array for project - app runtime nodes still reside as child of root node, not sure if i want to or easily can change that. also moved app runtime from config/config.php to lib/php/runtime.php, much clearer.

--- lib/php/runtime.php
+++ lib/php/runtime.php
@@ -2,31 +2,26 @@
 
 
 //This is different depending on which web/file.php is called. 
-$app_config=PROJECT_ROOT.DIRECTORY_SEPARATOR.'apps'.DIRECTORY_SEPARATOR.APP_NAME.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'config.php';
+$app_runtime=PROJECT_ROOT.DIRECTORY_SEPARATOR.'apps'.DIRECTORY_SEPARATOR.APP_NAME.DIRECTORY_SEPARATOR.'lib/php'.DIRECTORY_SEPARATOR.'runtime.php';
 
-if(is_file($app_config)) { 
-    require_once($app_config);
+if(is_file($app_runtime)) { 
+    require_once($app_runtime);
 }
 
 $path = $_SERVER['SCRIPT_NAME'];
 $path_prefix = dirname($_SERVER['SCRIPT_NAME']);
 //$app_prefix = "acc/".APP_NAME."/";
 $link_prefix = $path."?nid=".$app_prefix;
-// URL settings
-Flow::add("path_prefix",$path_prefix,false);
-Flow::add("link_prefix",$link_prefix,false);
 
-
-
-// Date Stuff
-Flow::add("right_now",date('Y-m-d H:i:s'),false);
-Flow::add("right_then",date('Y-m-d H:i:s', mktime(date('H'), date('i'), date('s')-1, date('m'), date('d'), date('Y'))),false);
-
-// User Stuff
+$right_now = date('Y-m-d H:i:s');
 $current_user_id = $_SESSION['NX_AUTH']['user_id'];
-Flow::add("current_user_id",$current_user_id,false);
+
+
+$runtime = array('path_prefix'=>$path_prefix,
+                'link_prefix'=>$link_prefix,
+                'right_now'=>$right_now,
+                'current_user_id'=>$current_user_id);
 
 
 
-
-
+Flow::add("runtime",$runtime,false);
