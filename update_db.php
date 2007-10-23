af097a32145e4138fbb50488d899b84243930ca2
Albert Lash <albert.lash@docunext.com>
2007-10-23 17:44:40 +0000

    much better database updating process, but should decide on whether to use file or database for database versioning numbers

--- update_db.php
+++ update_db.php
@@ -3,7 +3,9 @@
 /* Use this file to create the initial database, eventually this will move to 
 a web based installer.  */
 
-$db_version="0.04";
+$db_version = rtrim(file_get_contents('config/db_version.txt'));
+
+fwrite(STDOUT,"Current database version is ".$db_version."\n");
 
 /* Copy config/my_db_admin_settings.php.dist to my_db_admin_settings.php and change to your info */
 if(!is_file('config/my_db_admin_settings.php')) { 
@@ -15,23 +17,28 @@ if(!is_file('config/my_db_admin_settings.php')) {
 
 // should get db version first... 
 
-if ($db_version == "b") {
+if ($db_version == "a") {
     `echo "ALTER TABLE pb_accounts ADD COLUMN hide enum('off','on') default NULL;" | mysql -u $USER -p$PASS $DATABASE`;
     `echo "ALTER TABLE pb_entries ADD COLUMN status tinyint(2) default 2;" | mysql -u $USER -p$PASS $DATABASE`;
     // UPDATE to "c"
 }
 
-if($db_version == "c") { 
+if($db_version == "b") { 
     `echo "ALTER TABLE pb_accounts ADD COLUMN group_id tinyint(2) default NULL;" | mysql -u $USER -p$PASS $DATABASE`;
     fwrite(STDOUT,"Database update successful");
 }
 
-/* Not yet but soon: 
-if($db_version == "d") { 
+if($db_version == "c") { 
+    fwrite(STDOUT,"Dropping pb_account_parent_groups...\n");
     `echo "DROP TABLE pb_account_parent_groups;" | mysql -u $USER -p$PASS $DATABASE`;
-    fwrite(STDOUT,"Database update successful");
+    `echo "DELETE FROM pb_options WHERE option_key='pbooks_database_version';" | mysql -u $USER -p$PASS $DATABASE`; 
+    `echo "insert into pb_options ( option_key,option_value) VALUES ('pbooks_database_version','c');" | mysql -u $USER -p$PASS $DATABASE`;
+    `echo "d" > config/db_version.txt`;
+    fwrite(STDOUT,"Database update successful.\n");
 }
-    */
 
+if($db_version == "d") { 
+    fwrite(STDOUT,"No update required.\n");
+}
 ?>
 
