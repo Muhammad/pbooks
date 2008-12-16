44ad848c69cb038eb89db10420dd6a80a1b689b7
Albert Lash <albert.lash@docunext.com>
2008-12-16 22:26:54 +0000

    minor updates

--- web/user.php
+++ web/user.php
@@ -1,54 +1,34 @@
 <?php
 
-// Configuration 
-// Where is nexista? This path should be to a folder containing the nexista source 
-$nexista_path = "/var/www/dev/nexista/";
 
-// Bad hack
-if(!strpos($_SERVER['REQUEST_URI'],'.php')) { 
-    header("Location: ".$_SERVER['REQUEST_URI']."index.php");
-    exit;
-}
+// Where is nexista? This path should be to a folder containing nexista
+// This is the only thing you may need to edit:
+define('NX_PATH_BASE', "/var/www/dev/nexista/");
+
+
+
+
 $server_name = $_SERVER['SERVER_NAME'];
-define('SERVER_NAME',$server_name);
+define('SERVER_NAME', $server_name);
 $project_root = dirname(dirname(__FILE__));
-define('PROJECT_ROOT',$project_root);
-define('PROJECT_NAME','pbooks');
-define('APP_NAME','user');
+$project_root = str_replace('\\', '/', $project_root);
+define('NX_PATH_CORE', NX_PATH_BASE."kernel/");
+define('PROJECT_ROOT', $project_root);
+define('PROJECT_NAME', 'pbooks');
+define('APP_NAME', 'user');
 
 $server_init = PROJECT_ROOT."/cache/".SERVER_NAME."/".APP_NAME."/".APP_NAME.".php";
 
-
-
-if(!include($nexista_path.'/extensions/nx_builder.php')) {
-    // This is included just for transisioning 
-    if(!include($nexista_path.'/plugins/builder.php')) {
-        echo "Error: Unable to load server loader or builder.";
-        exit;
-    }
-    
-    // Loader not there or manually getting rebuilt? Build it!
-    if(!file_exists($server_init) || isset($_GET['nxbin'])) {
-        build_it_now();
-    } else { // Loader is there, check freshness, then either rebuild or include it.
-        check_freshness();
-            include($server_init);
-            exit;
-    }
-    // END TRANSITIONING
+if(!include(NX_PATH_BASE.'extensions/nexista_builder.php')) {
     echo "Error: Unable to load server loader or builder.";
     exit;
 }
 
-
-
-
 // Loader not there or manually getting rebuilt? Build it!
-if(!file_exists($server_init) || isset($_GET['nxbin'])) {
-    nexista_build_it_now();
+if(!file_exists($server_init) || isset($_POST['x--dev--rebuild'])) {
+    nexista_build_it_now($server_init);
 } else { // Loader is there, check freshness, then either rebuild or include it.
-    nexista_check_freshness();
+    nexista_check_freshness($server_init);
     include($server_init);
     exit;
 }
-
