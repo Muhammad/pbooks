af529bb0ad4b591e0850e7a5ffc3db302cf45209
Albert Lash <albert.lash@docunext.com>
2008-12-29 18:39:53 +0000

    fixed db install script to use demo dump

--- install_db.php
+++ install_db.php
@@ -1,6 +1,6 @@
 <?php
 
-/* Use this file to create the initial database, eventually this will move to 
+/* Use this file to create the initial database, eventually this will move to
 a web based installer.
 
 This script is not used with Windows or SQLite. */
@@ -9,13 +9,13 @@ This script is not used with Windows or SQLite. */
 if(!is_file('config/my_db_admin_settings.php')) { 
     echo "Copy config/my_db_admin_settings.php.dist to config/my_db_admin_settings.php and change to your info.\n";
     exit;
-} else {  
+} else {
     include('config/my_db_admin_settings.php');
 }
 
 `$MYSQLADMIN -u $ADMIN_USER -p$ADMIN_PASS CREATE $DATABASE`;
 
-`$MYSQLBIN -u $ADMIN_USER -p$ADMIN_PASS $DATABASE < ./apps/pbooks/data/sample_data/demo_pbooks_data.sql`;
+`$MYSQLBIN -u $ADMIN_USER -p$ADMIN_PASS $DATABASE < ./config/demo_db.sql`;
 
 `echo "GRANT ALL PRIVILEGES ON $DATABASE.* TO '$PBOOKS_USER'@'%' IDENTIFIED BY '$PBOOKS_PASS';" | $MYSQLBIN -u $ADMIN_USER -p$ADMIN_PASS $DATABASE`;
 
