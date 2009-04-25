afb2740f5358fb8bff4e380e819d6ce9b8b87084
Albert Lash <albert.lash@docunext.com>
2009-04-25 05:53:33 +0000

    simpler relative time for use with jquery

--- web/s/js/time/relative_time.js
+++ web/s/js/time/relative_time.js
@@ -1,51 +1,6 @@
-// Courtesy of Joey 
+// Courtesy of Joey
 // Public Domain Code
 // http://joey.kitenet.net/blog/entry/relative_dates_in_html/
-
-var dateElements;
-window.onload = getDates;
-
-function getDates() {
-    dateElements = getElementsByClass('date');
-    for (var i = 0; i < dateElements.length; i++) {
-        var elt = dateElements[i];
-        var title = elt.attributes.title;
-        var d = new Date(title ? title.value : elt.innerHTML);
-        if (! isNaN(d)) {
-            dateElements[i].date=d;
-            elt.title=elt.innerHTML;
-        }
-    }
-
-    showDates();
-}
-
-function showDates() {
-    for (var i = 0; i < dateElements.length; i++) {
-        var elt = dateElements[i];
-        var d = elt.date;
-        if (! isNaN(d)) {
-            elt.innerHTML=relativeDate(d);
-        }
-    }
-    setTimeout(showDates,30000); // keep updating every 30s
-}
-
-function getElementsByClass(cls, node, tag) {
-        if (document.getElementsByClass)
-                return document.getElementsByClass(cls, node, tag);
-        if (! node) node = document;
-        if (! tag) tag = '*';
-        var ret = new Array();
-        var pattern = new RegExp("(^|\\s)"+cls+"(\\s|$)");
-        var els = node.getElementsByTagName(tag);
-        for (i = 0; i < els.length; i++) {
-                if ( pattern.test(els[i].className) ) {
-                        ret.push(els[i]);
-                }
-        }
-        return ret;
-}
 
 var timeUnits = new Array;
 timeUnits['minute'] = 60;
