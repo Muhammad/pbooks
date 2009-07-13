d8cb9f9f457effc4bd26f83f3fcd962afaa3d82b
Albert Lash <albert.lash@docunext.com>
2009-07-13 04:26:40 +0000

    This ledger export script is coming along nicely. Lua is quite easy to write; it reminds me a little of bash.
    Eventually I'd like an interactive command line interface to PBooks, similar to the trac-admin tool.

--- apps/pbooks/scripts/lua/ledger_export.lua
+++ apps/pbooks/scripts/lua/ledger_export.lua
@@ -7,18 +7,18 @@ Author: Albert L. Lash, IV
 License: Gnu Affero Public License version 3
 http://www.gnu.org/licenses
 
-This program is free software; you can redistribute it and/or modify
+This program is free software you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published by
-the Free Software Foundation; either version 3 of the License, or
+the Free Software Foundation either version 3 of the License, or
 (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
+but WITHOUT ANY WARRANTY without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU Affero General Public License for more details.
 
 You should have received a copy of the GNU Affero General Public License
-along with this program; if not, see http://www.gnu.org/licenses
+along with this program if not, see http://www.gnu.org/licenses
 or write to the Free Software Foundation, Inc., 51 Franklin Street,
 Fifth Floor, Boston, MA 02110-1301 USA
 --> --]]
@@ -36,15 +36,47 @@ function rows (connection, sql_statement)
 end
 
 myledger = ""
+account_type = ""
 
-for transaction_id, entry_datetime,memorandum in rows (con, "select transaction_id, SUBSTRING(entry_datetime,1,10), SUBSTRING(memorandum,1,40) from pb_general_ledger LIMIT 20") do
+myquery = [[SELECT entry_id,
+    SUBSTRING(entry_datetime,1,10) as entry_datetime,
+    SUBSTRING(memorandum,1,30) as memorandum
+  FROM pb_entries
+  WHERE entry_id!=0
+    AND memorandum!='PLACEHOLDER'
+    AND pb_entries.status != 9
+  LIMIT 30]]
+
+getallentries = [[SELECT 
+    pb_entry_amounts.entry_amount,
+    RPAD(pb_accounts.name,35,' ') as name,
+    pb_entry_amounts.memorandum as entry_amount_memorandum,
+    pb_entry_amounts.entry_type_id
+  FROM
+    (pb_entries)
+  LEFT JOIN pb_entry_amounts
+    ON pb_entry_amounts.entry_id=pb_entries.entry_id
+  LEFT JOIN pb_accounts
+    ON pb_entry_amounts.account_id=pb_accounts.id
+  WHERE
+    pb_entries.entry_id=]]
+
+
+for entry_id,entry_datetime,memorandum in rows (con, myquery) do
 -- 2007-01-02 'AUTOMATIC LOAN PAY' 
 --  Expenses:Expense Account 1               		$4
 --  Assets:Bank Account1                       			$-4
 
   myledger = myledger..entry_datetime.." '"..memorandum.."'\n"
-  myledger = myledger.."  "..transaction_id.."\n"
+  getentryamounts = getallentries..entry_id
 
-  print (string.format ("%s", myledger))
+  for entry_amount,account_name,entry_amount_memorandum,entry_type_id in rows (con, getentryamounts) do
+    myledger = myledger.."  "..account_type..":"..account_name.."    "
+    if entry_type_id == "Credit" then
+        myledger = myledger.."    "
+    end
+    myledger = myledger..entry_amount.."\n"
+  end
 end
+print (string.format ("%s", myledger))
 
